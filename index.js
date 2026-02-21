e0f370844bad303f10ac0c256e9a3dae028f120a
Anirban._.Roy <www.anirbanroy@gmail.com>
2026-02-21 14:43:24 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,33 @@
 const express = require('express');
 const compression = require('compression');
-const path = require('path');
 
 const app = express();
-
 
 app.use(compression({
     level: 6,
     threshold: 1024,
 }));
 
-app.use(express.static(path.join(__dirname, 'public'), {
+app.use(express.static('public', {
     maxAge: '7d',
     etag: true,
     lastModified: true
 }));
 
-
 const projects = ['Gravitas', 'Typemaster', 'Spaceshooter'];
 
-
 projects.forEach(project => {
-    app.get([`/${project}`, `/${project}/`], (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', project.toLowerCase(), 'index.html'));
+    app.get(`/${project}`, (req, res) => {
+        res.sendFile(__dirname + `/public/${project.toLowerCase()}/index.html`);
     });
 });
 
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+    res.status(404).sendFile(__dirname + '/public/404.html');
 });
 
-// Start server
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
